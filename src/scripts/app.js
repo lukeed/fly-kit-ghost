c822a52ef28a3142bcecb16449324d12822bbdac
luke <luke.edwards05@gmail.com>
2016-03-20 22:25:52 -0700

    passing scripts for linter

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -10,10 +10,10 @@ if (isOk) {
 /**
  * Main JS file for Casper behaviours
  */
-$(document).ready(function() {
+$(document).ready(function () {
 	$('.post-content').fitVids();
-	$('.scroll-down').arctic_scroll();
-	$('.menu-button, .nav-cover, .nav-close').on('click', function(e) {
+	$('.scroll-down').arcticScroll();
+	$('.menu-button, .nav-cover, .nav-close').on('click', function (e) {
 		e.preventDefault();
 		$('body').toggleClass('nav-opened nav-closed');
 	});
@@ -21,27 +21,27 @@ $(document).ready(function() {
 
 // Arctic Scroll by Paul Adam Davis
 // https://github.com/PaulAdamDavis/Arctic-Scroll
-$.fn.arctic_scroll = function(options) {
+$.fn.arcticScroll = function (options) {
 	options = $.extend({
 		elem: $(this),
 		speed: 500
 	}, options);
 
-	options.elem.click(function(event) {
+	options.elem.click(function (event) {
 		event.preventDefault();
-		var $this = $(this),
-			$htmlBody = $('html, body'),
-			offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false,
-			position = ($this.attr('data-position')) ? $this.attr('data-position') : false,
-			toMove;
+		var $this = $(this);
+		var $htmlBody = $('html, body');
+		var offset = ($this.attr('data-offset')) ? $this.attr('data-offset') : false;
+		var position = ($this.attr('data-position')) ? $this.attr('data-position') : false;
+		var toMove;
 
 		if (offset) {
-			toMove = parseInt(offset);
+			toMove = parseInt(offset, 10);
 			$htmlBody.stop(true, false).animate({
 				scrollTop: ($(this.hash).offset().top + toMove)
 			}, options.speed);
 		} else if (position) {
-			toMove = parseInt(position);
+			toMove = parseInt(position, 10);
 			$htmlBody.stop(true, false).animate({
 				scrollTop: toMove
 			}, options.speed);
